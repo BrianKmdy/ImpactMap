a87929be99bf0b840ab4a6a901b6ccaa1f5d1f87
Zijian Chen <zijianchan@gmail.com>
2016-05-30 23:05:34 -0700

    Picture Upload Changed

--- php/admin/projects/submit_project_edit.php
+++ php/admin/projects/submit_project_edit.php
@@ -12,13 +12,23 @@
 
 	require_once "../../common/dbConnect.php";
 	require_once "../../common/class.map.php";
+	require_once "../../common/class.aws.php";
 
 	$map = new Map();
-	if (isset($_POST['pid'], $_POST['cid'], $_POST['title'], $_POST['status'], $_POST['startDate'], $_POST['endDate'], $_POST['buildingName'], $_POST['address'], $_POST['zip'], $_POST['type'], $_POST['summary'], $_POST['results'], $_POST['link'], $_POST['pic'], $_POST['conid'], $_POST['fundedBy'], $_POST['keywords'], $_POST['stemmedSearchText'], $_POST['visible'], $_POST['lat'], $_POST['lng'])) {
+	$aws = new AWS();
+	if (isset($_POST['pid'], $_POST['cid'], $_POST['title'], $_POST['status'], $_POST['startDate'], $_POST['endDate'], $_POST['buildingName'], $_POST['address'], $_POST['zip'], $_POST['type'], $_POST['summary'], $_POST['results'], $_POST['link'], $_POST['conid'], $_POST['fundedBy'], $_POST['keywords'], $_POST['stemmedSearchText'], $_POST['visible'], $_POST['lat'], $_POST['lng'])) {
 		if (intval($_POST['pid']) == -1) {
 			$map -> add_project();
+			if (isset($_FILES['pic'])) {
+				$url = $aws->upload_object($_POST['pid']);
+				$map->save_picture($_POST['pid'], $url);
+			}
 			echo "Databse updated";
 		} else {
+			if (isset($_POST['pic'])) {
+				$aws -> delete_object($_POST['pid']);
+				$aws -> upload_object($_POST['pid']);
+			}
 		    $map -> update_project();
 		    echo "Databse updated";
 		}
